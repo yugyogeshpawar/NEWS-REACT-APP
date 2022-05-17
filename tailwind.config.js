886c593910d544bacfaf0662ea834f3dec7ea3df
Yogesh Pathe <98703537+yugyogeshpawar@users.noreply.github.com>
2022-05-17 21:43:40 +0530

    main

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+    theme: {
+      fontFamily: {
+        'proxima': ['Proxima Nova','sans-serif'],
+      }      
+    }
+  },
+  plugins: [
+
+  ],
+}
